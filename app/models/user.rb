5036f5a9a372ec879b5daa1642ea20df3b8fd109
Hau Phan T. VN.Danang <hau.phan@monstar-lab.com>
2022-06-01 09:09:41 +0700

    01/06/2022, hau phan : fix comment login logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
+  has_secure_password
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
+  enum role: [:renter, :lessor]
+
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
-  
-  has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   def User.digest(string)
