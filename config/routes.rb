288549869e9315f66825181866162e69ae93cb5e
Hau Phan T. VN.Danang <hau.phan@monstar-lab.com>
2022-06-16 08:51:28 +0700

    fix comement + add cancel button

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  patch 'cancel/:id', to: 'bikes#cancel', as: 'cancel'
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
