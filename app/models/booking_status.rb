ea031b95fd8180545933f6def15d0b5c18d52105
Hau Phan T. VN.Danang <hau.phan@monstar-lab.com>
2022-06-29 10:13:41 +0700

    27/06/2022, hau phan : payment

--- app/models/booking_status.rb
+++ app/models/booking_status.rb
@@ -2,5 +2,5 @@ class BookingStatus < ApplicationRecord
   belongs_to :booking
   belongs_to :user
 
-  enum status: [:pending, :booking, :cancel, :finished]
+  enum status: [:pending, :booking, :cancel, :payment, :finished]
 end
