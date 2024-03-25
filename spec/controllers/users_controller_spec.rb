700bcd8f136c9f97f42aa1fdf6a86a9f5fc5854d
Na Le T <na.le@monstar-lab.com>
2024-03-25 11:29:33 +0700

    fix comment

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,6 +5,7 @@ include AuthenticationSpecHelper
   before do
     @controller = UsersController.new
   end
+
   describe 'GET #new' do   
     context 'Render template new' do
       it 'renders the `new` template' do
@@ -13,45 +14,72 @@ include AuthenticationSpecHelper
       end
     end
   end
+
   describe 'POST #create' do  
-    let!(:user) { create(:user) }
+    let(:valid_user_params) do
+    {
+      name: 'valid_name',
+      email: 'valid_email@example.com',
+      password: 'valid_password',
+      password_confirmation: 'valid_password',
+      role: :renter ,
+      phone_number: '12345678'
+    }
+    end
+
     context 'create with valid user' do
       it 'create a new user' do
-        params = {
-          name: user.name,
-          email: 'testuser@gmail.com',
-          password: user.password_digest,
-          password_confirmation: user.password_digest,
-          role: user.role ,
-          phone_number: user.phone_number
-        }
-        expect { post(:create, params: { user: params }) }.to change(User, :count).by(1)
+        expect { post(:create, params: { user: valid_user_params }) }.to change(User, :count).by(1)
         expect(flash[:info]).to eq('Please check your email to activate your account.')
         expect(response).to have_http_status(303)
         expect(response).to redirect_to(root_url)
       end
     end
+
     context 'create with invalid user' do
-      it 'renders the new template and status' do
-        params = {
-          name: user.name,
-          email: 'email_wrong',
-          password: user.password_digest,
-          password_confirmation: user.password_digest,
-          role: user.role ,
-          phone_number: user.phone_number
-        }
-        post :create, params: { user: params }
+      it 'case name, email is nil' do
+        post :create, params: { user: attributes_for(:user, email: nil, name: nil) }
         expect(response).to render_template('new')
+      end
+
+      it 'case email has wrong format' do
+        post :create, params: { user: attributes_for(:user, email: "invalid_email") }
         expect(response).to have_http_status(422)
+      end
+
+      it "case duplicate emails" do
+        user = create(:user, email: "example@email.com")
+        expect {
+          post :create, params: { user: attributes_for(:user, email: "example@email.com") }
+        }.to_not change(User, :count)
+      end
+
+      it "case password length less than 6" do
+        expect {
+          post :create, params: { user: attributes_for(:user, password:1234) }
+        }.to_not change(User, :count)
+      end
+
+      it "case name length greater than 50" do
+        expect {
+          post :create, params: { user: attributes_for(:user, name:Faker::Lorem.characters(number: 60)) }
+        }.to_not change(User, :count)
+      end
+
+      it "case email length greater than 255" do
+        expect {
+          post :create, params: { user: attributes_for(:user, email:Faker::Lorem.characters(number: 256) + "@example.com") }
+        }.to_not change(User, :count)
       end
     end
   end
+
   describe 'PUT #update' do
     let!(:user) { create :user }
     before do
       login_as(user)
     end
+
     context 'Update with valid user' do
       it 'update user info' do
         params = {
@@ -66,14 +94,38 @@ include AuthenticationSpecHelper
       expect(flash[:success]).to eq 'User profile updated'
       end
     end
+
     context 'update with invalid user' do
-      it 'render edit template' do
-        params = {
-          email: 'test',
-        }
-        put :update, params: { id: user.id, user: params }
-        expect(response).to render_template('edit')
+      it 'case edit name, email is nil' do
+        put :update, params: { id: user.id, user: {name: nil, email: nil} }
+        expect(user.email).not_to eq(nil)
+        expect(user.name).not_to eq(nil)
+      end
+
+      it 'case edit email has wrong format' do
+        put :update, params: { id: user.id, user: {email: "invalid_email"} }
+        expect(user.email).not_to eq("invalid_email")
+      end
+
+      it "case edit duplicate emails" do
+        user = create(:user, email: "example@email.com")
+        put :update, params: { id: user.id, user: {email: "example@email.com"} }
         expect(response).to have_http_status(303)
+      end
+
+      it "case edit password length less than 6" do
+        put :update, params: { id: user.id, user: {password:1234} }
+        expect(response).to have_http_status(303)
+      end
+
+      it "case edit name length greater than 50" do
+        put :update, params: { id: user.id, user: {name:Faker::Lorem.characters(number: 60)} }
+        expect(response).to render_template('edit')
+      end
+
+      it "case edit email length greater than 255" do
+        put :update, params: { id: user.id, user: { email:Faker::Lorem.characters(number: 256) + "@example.com"} }
+        expect(response).to render_template('edit')
       end
     end
   end
