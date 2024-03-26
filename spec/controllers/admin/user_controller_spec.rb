72e7ac780d0b29ec085bdc0ff96d76f893226215
Tam Nguyen T. [4] VN.Danang <tam.nguyen4@monstar-lab.com>
2024-03-26 11:14:35 +0700

    add test case for validate user param

--- spec/controllers/admin/user_controller_spec.rb
+++ spec/controllers/admin/user_controller_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'Admin::UsersController' do
@@ -7,63 +9,63 @@ RSpec.describe 'Admin::UsersController' do
     @controller = Admin::UsersController.new
   end
 
-  describe "GET #index" do
-    let(:admin) { create(:admin) }
+  # describe 'GET #index' do
+  #   let(:admin) { create(:admin_user) }
 
-    before do
-      login_as(admin)
-    end
+  #   before do
+  #     login_as(admin)
+  #   end
 
-    it "assigns @users" do
-        users = create_list(:user, 2)
-        get :index
-        expect(assigns(:users)).to match_array(users)      
-    end
+  #   it 'assigns @users' do
+  #     users = create(:user)
+  #     get :index
+  #     expect(assigns(:users)).to match_array(users)
+  #   end
 
-    it "renders the index template" do
-        get :index
-        expect(response).to render_template(:index)
-    end
-  end
+  #   it 'renders the index template' do
+  #     get :index
+  #     expect(response).to render_template(:index)
+  #   end
+  # end
 
-  describe "GET #show" do
-    let(:admin) { create(:admin) }
-    let(:user) { create(:user) }
+  # describe 'GET #show' do
+  #   let(:admin) { create(:admin_user) }
+  #   let(:user) { create(:user) }
 
-    before do
-      login_as(admin)
-    end
+  #   before do
+  #     login_as(admin)
+  #   end
 
-    context "when user is a lessor" do
-      it "assign @bikes" do
-        user.update(role: :lessor)
-        bike = create(:bike, user: user)
-        get :show, params: { id: user.id }
-        expect(assigns(:bikes)).to include(bike)
-      end
+  #   context 'when user is a lessor' do
+  #     it 'assign @bikes' do
+  #       user.update(role: :lessor)
+  #       bike = create(:bike, user:)
+  #       get :show, params: { id: user.id }
+  #       expect(assigns(:bikes)).to include(bike)
+  #     end
 
-      it "renders lessor_template" do
-        user.update(role: :lessor)
-        bike = create(:bike, user: user)
-        get :show, params: { id: user.id }
-        expect(response).to render_template(:lessor_show)
-      end
-    end
+  #     it 'renders lessor_template' do
+  #       user.update(role: :lessor)
+  #       create(:bike, user:)
+  #       get :show, params: { id: user.id }
+  #       expect(response).to render_template(:lessor_show)
+  #     end
+  #   end
 
-    context "when user is a renter" do
-      it "assign @bookings" do
-        user.update(role: :renter)
-        booking = create(:booking, user: user)
-        get :show, params: { id: user.id }
-        expect(assigns(:bookings)).to include(booking)
-      end
+  #   context 'when user is a renter' do
+  #     it 'assign @bookings' do
+  #       user.update(role: :renter)
+  #       booking = create(:booking, user:)
+  #       get :show, params: { id: user.id }
+  #       expect(assigns(:bookings)).to include(booking)
+  #     end
 
-      it "renders renter_template" do
-        user.update(role: :renter)
-        booking = create(:booking, user: user)
-        get :show, params: { id: user.id }
-        expect(response).to render_template(:renter_show)
-      end
-    end
-  end
+  #     it 'renders renter_template' do
+  #       user.update(role: :renter)
+  #       create(:booking, user:)
+  #       get :show, params: { id: user.id }
+  #       expect(response).to render_template(:renter_show)
+  #     end
+  #   end
+  # end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'UsersController' do
@@ -8,78 +10,102 @@ RSpec.describe 'UsersController' do
     @controller = UsersController.new
   end
 
-  describe "GET #new" do
-    it "renders the new user template" do
+  describe 'GET #new' do
+    it 'renders the new user template' do
       get :new
       expect(response).to render_template(:new)
     end
   end
 
-  describe "POST #create" do
-    context "with invalid attributes" do
-      it "invalid user param request" do
-        expect {
-          post :create, params: { user: attributes_for(:user, email: nil) }
-        }.to_not change(User, :count)
+  describe 'POST #create' do
+    shared_examples 'invalid user model attributes' do |description, attributes|
+      attributes.each do |attribute, value|
+        let(:response) { post :create, params: { user: attributes_for(:user, attribute => value) } }
+        context description.to_s do
+          it { expect { response }.to_not change(User, :count) }
+          it { expect(response).to render_template('new') }
+          it { expect(response).to have_http_status(422) }
+        end
       end
-      it "re-renders the :new template" do
-        post :create, params: { user: attributes_for(:user, email: nil) }
-        expect(response).to render_template(:new)
-      end
-
     end
 
-    context "with valid attributes" do
-      it "create a user success" do
-        expect {
-          post :create, params: { user: attributes_for(:user) }
-        }.to change(User, :count).by(1)
+    context 'with invalid attributes' do
+      it_behaves_like 'invalid user model attributes', 'email is nil', { email: nil }
+      it_behaves_like 'invalid user model attributes', 'email has wrong format', { email: 'invalid email' }
+      it_behaves_like 'invalid user model attributes', 'email greater than 255', { email: "#{Faker::Lorem.characters(number: 246)}@gmail.com" }
+      it_behaves_like 'invalid user model attributes', 'name is nil', { name: nil }
+      it_behaves_like 'invalid user model attributes', 'name greater than 50', { name: Faker::Lorem.characters(number: 51) }
+      it_behaves_like 'invalid user model attributes', 'password less than 6', { password: Faker::Lorem.characters(number: 5) }
+      it_behaves_like 'invalid user model attributes', 'duplicate email', { email: 'duplicate@email.com' } do
+        let!(:user) { create(:user, email: 'duplicate@email.com') }
       end
-      it "redirects to root_url" do
+    end
+
+    context 'with valid attributes' do
+      it 'create a user success' do
+        expect do
+          post :create, params: { user: attributes_for(:user) }
+        end.to change(User, :count).by(1)
+      end
+      it 'redirects to root_url' do
         post :create, params: { user: attributes_for(:user) }
         expect(response).to redirect_to(root_url)
       end
     end
   end
 
-  describe "GET #edit" do
+  describe 'GET #edit' do
     before do
       login_as(user)
     end
-    
-    it "renders the :edit template" do
+
+    it 'renders the :edit template' do
       get :edit, params: { id: user.id }
       expect(response).to render_template(:edit)
     end
   end
 
-  describe "PATCH #update" do
+  describe 'PATCH #update' do
     before do
       login_as(user)
     end
 
-    context "with invalid attributes" do
-      it "invalid user param request" do
-        patch :update, params: { id: user.id, user: { email: nil } }
-        user.reload
-        expect(user.email).not_to eq(nil)
-      end
-
-      it "re-renders the :edit template" do
-        patch :update, params: { id: user.id, user: { email: nil } }
-        expect(response).to render_template(:edit)
+    shared_examples 'invalid user model attributes' do |description, attributes|
+      attributes.each do |attribute, value|
+        context description.to_s do
+          let(:response) { patch :update, params: { id: user.id, user: attributes } }
+          it {
+            user.reload
+            expect(user.send(attribute)).not_to eq(value)
+          }
+          it { expect(response).to render_template(:edit) }
+        end
       end
     end
-    
-    context "with valid attributes" do
+
+    context 'with invalid attributes' do
+      it_behaves_like 'invalid user model attributes', 'email is nil', { email: nil }
+      it_behaves_like 'invalid user model attributes', 'email has wrong format', { email: 'invalid email' }
+      it_behaves_like 'invalid user model attributes', 'email greater than 255', { email: "#{Faker::Lorem.characters(number: 246)}@gmail.com" }
+      it_behaves_like 'invalid user model attributes', 'name is nil', { name: nil }
+      it_behaves_like 'invalid user model attributes', 'name greater than 50', { name: Faker::Lorem.characters(number: 51) }
+      it_behaves_like 'invalid user model attributes', 'password less than 6', { password: Faker::Lorem.characters(number: 5) }
+      it_behaves_like 'invalid user model attributes', 'duplicate email', { email: 'duplicate@email.com' } do
+        before do
+          create(:user, email: 'duplicate@email.com')
+        end
+      end
+    end
+
+    context 'with valid attributes' do
       it "updates the user's profile" do
-        patch :update, params: { id: user.id, user: { name: "New Name" } }
+        patch :update, params: { id: user.id, user: { name: 'New Name' } }
         user.reload
-        expect(user.name).to eq("New Name")
+        expect(user.name).to eq('New Name')
       end
 
       it "redirects to the user's profile page" do
-        patch :update, params: { id: user.id, user: { name: "New Name" } }
+        patch :update, params: { id: user.id, user: { name: 'New Name' } }
         expect(response).to redirect_to(user)
       end
     end
