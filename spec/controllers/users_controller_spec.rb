b76ceba01aff8faaf604b6c1ee186ad2081f12b2
Ha Phan T <ha.phan@monstar-lab.com>
2024-03-26 10:54:58 +0700

    fix comment

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -14,32 +14,28 @@ RSpec.describe 'UsersController' do
       login_as(user)
     end
 
-    it "assigns the requested user to @user" do
+    it "render the :new template" do
       get :new
       expect(response).to render_template("new")
     end
     
   end
 
-  describe "POST #new" do
-    let(:user) { create(:user) }
-
-    before do
-      login_as(user)
+  describe "POST #create" do
+    let(:valid_user_params) do
+      {
+        name: 'valid_name',
+        email: 'valid_email@example.com',
+        password: 'valid_password',
+        password_confirmation: 'valid_password',
+        role: :renter ,
+        phone_number: '12345678'
+      }
     end
-
-    it "create a user successfully" do
-      post :create, params: { user: {
-          name: Faker::Name.name, 
-          email: "alone.hht@gmail.com", 
-          password: user.password,
-          password_confirmation: user.password,
-          activated_at: Time.zone.now,
-          role: user.role
-        }
-      } 
-
-      expect(response).to have_http_status(303)
+    context 'create with valid user' do
+      it 'create a new user' do
+        expect { post(:create, params: valid_user_params) }.to have_http_status(303)
+      end
     end
 
     it "fail to create a user" do
